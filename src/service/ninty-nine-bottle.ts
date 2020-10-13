8e169b6162edc77f7cff02bcd2fc5b0b19216156
Travis Craft <tlcraft@users.noreply.github.com>
2020-10-13 14:59:49 -0600

    Flesh out song.

--- src/service/ninty-nine-bottle.ts
+++ src/service/ninty-nine-bottle.ts
@@ -5,13 +5,31 @@ export interface INintyNineBottles {
 export class NintyNineBottles implements INintyNineBottles {
     singSong(numberOfBottles = 99): void {
         while (numberOfBottles) {
-            console.log(
-                `${numberOfBottles} bottles of beer on the wall, ${numberOfBottles} bottles of beer.`
-            );
-            numberOfBottles--;
-            console.log(
-                `Take one down, pass it around, ${numberOfBottles} bottles of beer on the wall...`
-            );
+            if (numberOfBottles >= 3) {
+                console.log(
+                    `${numberOfBottles} bottles of beer on the wall, ${numberOfBottles} bottles of beer.`
+                );
+                numberOfBottles--;
+                console.log(
+                    `Take one down, pass it around, ${numberOfBottles} bottles of beer on the wall...`
+                );
+            } else if (numberOfBottles === 2) {
+                console.log(
+                    `${numberOfBottles} bottles of beer on the wall, ${numberOfBottles} bottles of beer.`
+                );
+                numberOfBottles--;
+                console.log(
+                    `Take one down, pass it around, ${numberOfBottles} bottle of beer on the wall...`
+                );
+            } else {
+                console.log(
+                    `${numberOfBottles} bottle of beer on the wall, ${numberOfBottles} bottle of beer.`
+                );
+                numberOfBottles--;
+                console.log(
+                    'Take one down, pass it around, no more bottles of beer on the wall...'
+                );
+            }
         }
 
         console.log(
