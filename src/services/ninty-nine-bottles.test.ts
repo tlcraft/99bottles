9a9f98501bc507156dc2872b82c018cd15733021
Travis Craft <tlcraft@users.noreply.github.com>
2020-10-16 14:17:24 -0600

    Update tests.

--- src/services/ninty-nine-bottles.test.ts
+++ src/services/ninty-nine-bottles.test.ts
@@ -1,6 +1,13 @@
 import {NintyNineBottles} from './ninty-nine-bottles';
 
 describe('ninty-nine-bottles', () => {
+    test('should log 4 times when the number of bottles start at 1', () => {
+        const logSpy = spyOn(console, 'log');
+        const nintyNineBottles = new NintyNineBottles();
+        nintyNineBottles.singSong(1);
+        expect(logSpy).toHaveBeenCalledTimes(4);
+    });
+
     test('should log 6 times when the number of bottles start at 2', () => {
         const logSpy = spyOn(console, 'log');
         const nintyNineBottles = new NintyNineBottles();
@@ -8,10 +15,10 @@ describe('ninty-nine-bottles', () => {
         expect(logSpy).toHaveBeenCalledTimes(6);
     });
 
-    test('should log 4 times when the number of bottles start at 1', () => {
+    test('should log 200 times when the number of bottles start at 99', () => {
         const logSpy = spyOn(console, 'log');
         const nintyNineBottles = new NintyNineBottles();
-        nintyNineBottles.singSong(1);
-        expect(logSpy).toHaveBeenCalledTimes(4);
+        nintyNineBottles.singSong(99);
+        expect(logSpy).toHaveBeenCalledTimes(200);
     });
 });
